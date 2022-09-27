56537f2ceeeb7ca6849569ebacdbed5bb801e786
Brice <bricel67@gmail.com>
2022-09-27 15:16:33 +0200

    add category from ui

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    debugger
     @article = Article.new(article_params)
     @article.user = current_user
 
@@ -53,7 +54,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user

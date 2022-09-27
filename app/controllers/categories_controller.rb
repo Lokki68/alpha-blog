1150fe37806a8bd90253e350a85a71cbe1b6b264
Brice <bricel67@gmail.com>
2022-09-27 16:04:39 +0200

    edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,6 +25,23 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      flash[:notice] = "Category name has been updated successfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+  
+  
+
   private
 
   def category_params
